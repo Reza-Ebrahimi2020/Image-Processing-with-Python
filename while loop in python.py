bfadac6ca78045e59d50f888c361597dabff2817
Reza Ebrahimi <78690714+Reza-Ebrahimi2020@users.noreply.github.com>
2022-05-19 08:29:06 +0500

    Create while loop in python
    while loop

---
+++ while loop in python.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May 19 08:23:27 2022
+
+@author: Reza
+"""
+
+count = 0
+
+while count <= 10:
+    print("The Current count is :", count)
+    if count == 5:
+        break
+    count = count + 1
